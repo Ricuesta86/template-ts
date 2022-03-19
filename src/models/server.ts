7cf47c2d5061f2b3dfcbd6147776e52797118b2e
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2022-03-19 10:00:36 -0400

    feat: add controllers routers usuario

--- src/models/server.ts
+++ src/models/server.ts
@@ -1,16 +1,26 @@
 import express, { Application } from 'express';
+import routerUsuario from '../routers/usuario';
 
 export class Server {
 
     private app: Application;
     private port: string;
+    private apiPaths = {
+        usuarios: '/api/usuarios'
+    };
 
     constructor () {
         this.app=express();
         this.port = process.env.PORT || '3000';
+
+
+        //Definir todas las rutas
+        this.router();
     }
 
-
+    router(){
+        this.app.use( this.apiPaths.usuarios, routerUsuario );
+    }
 
     listen () {
         this.app.listen( this.port, ()=>{
