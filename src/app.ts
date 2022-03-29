e7b6f57ef719f6ff04e99e53924a538d1ff85a6b
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2022-03-29 13:25:18 -0500

    feat: add middleware

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,22 @@
 import  express ,{ Application} from 'express';
 import routerUsuario from './routers/usuario';
+import cors from 'cors';
+import path from 'path';
 
 
 const app: Application = express();
 
+//Middleware
+
+//Cors
+app.use(cors());
+
+//Lectura del body
 app.use(express.json());
-app.use(express.urlencoded());
+//app.use(express.urlencoded());
+
+//Carpeta pública
+app.use( express.static(path.resolve(__dirname,'public')));
 
 app.use('/api/usuarios', routerUsuario);
 
